22fcac3c61a07178247ede2181f3a5aeba0656d9
Cristiane Reis <anni.nanne@gmail.com>
2024-11-03 15:21:38 -0300

    treino Funcao

---
+++ funcao.js
@@ -0,0 +1,7 @@
+function calculoArea(base, altura) {
+    const area =  base * altura;
+    return `O valor da área é: ${area}`;
+
+}
+console.log(calculoArea(5, 10))
+console.log(calculoArea(15, 10))
